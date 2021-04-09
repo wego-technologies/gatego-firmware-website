f58d4e59b04c439ea9dbe508c63f163cf0fcb9b2
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-04-09 13:02:33 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,6 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done()); 
 Router.events.on('routeChangeError', () => NProgress.done());
 
-
-
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
